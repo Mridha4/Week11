9ddced48d5b2da4f15cfcb2e745af75be4386cb1
Saad Al-Mridha <salmridha44@gmail.com>
2022-11-15 14:54:45 -0800

    function call

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// function greet (user) {
+//     console.log(`Whats up guys its yah boi ${user}`);
+
+// }
+// greet("Saad");
+
+
+// const greet = function (user) {
+//     console.log(`Hello ${user}`);
+// }
+// greet("Saad");
+
+// // Arrow functions
+// const greet = (user) => {
+//     console.log(`Hello ${user}`);
+// }
+// greet("Saad");
+
+
+function greet (user, time = "day") {
+    console.log(`Good ${time} ${user}`);
+
+}
+greet("Saad");
+greet("Saad", "Night");
